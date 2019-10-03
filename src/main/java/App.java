7c58b28f3e632ab98bcfcd967b5a9bb38c8911ee
Eliane-5 <irankundaeliane9@gmail.com>
2019-10-03 19:29:33 +0200

    adding styling to lyout and index pages

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,6 +6,7 @@ import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args){
+        staticFileLocation("/public");
 //        making a home pathway
         get("/",(request, response) ->{
             Map<String, Object> model = new HashMap<>();
